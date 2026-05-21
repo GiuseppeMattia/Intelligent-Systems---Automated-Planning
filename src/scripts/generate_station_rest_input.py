d310bfda2e8756cbb4539b640df6d13f4682ed49
maperri <perrimarco04@gmail.com>
2026-05-21 17:05:06 +0200

    add script to generate min and max rest time for each station

---
+++ src/scripts/generate_station_rest_input.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+from dotenv import load_dotenv
+
+def generate_station_rest_input():
+    '''
+    Script to generate asp facts. In the result, for each station there is the minimum and maximum rest time.
+    '''
+
+    load_dotenv()
+
+    df = pd.read_csv(os.getenv('PATH_TO_STOP_TIMES_CSV'))
+
+    df['arrival_mins'] = pd.to_timedelta(df['arrival_time']).dt.total_seconds() // 60
+    df['departure_mins'] = pd.to_timedelta(df['departure_time']).dt.total_seconds() // 60
+
+    df = df.sort_values(['trip_id', 'stop_sequence'])
+
+    df['halt_mins'] = df['departure_mins'] - df['arrival_mins']
+
+    rest_df = df.groupby('stop_id')['halt_mins'].agg(['min', 'max']).reset_index()
+
+    with open(os.getenv('PATH_TO_REST_INPUT'), 'w') as file:
+        file.write(f'%% rest(Station_id, Min_rest, Max_rest).\n\n')
+        for row in rest_df.itertuples(index=False):
+            encoded_row = f'rest({row.stop_id}, {row.min}, {row.max}).\n'
+            file.write(encoded_row)
+
+
+generate_station_rest_input()
+
--- src/scripts/generate_stoptimes_input.py
+++ src/scripts/generate_stoptimes_input.py
@@ -1,4 +1,5 @@
-import pandas, os
+import pandas as pd
+import os
 from dotenv import load_dotenv
 
 def generate_stoptimes_input():
@@ -8,15 +9,17 @@ def generate_stoptimes_input():
 
     load_dotenv()
 
-    df = pandas.read_csv(os.getenv('PATH_TO_STOP_TIMES_CSV'))
+    df = pd.read_csv(os.getenv('PATH_TO_STOP_TIMES_CSV'))
 
-    new_df = df[['trip_id']].copy()
-    new_df['station_id'] = df['stop_id']
-    new_df['stop_sequence'] = df['stop_sequence']
-    new_df.insert(0, 'stop_id', range(1, len(new_df) + 1))
+    stop_df = pd.DataFrame()
+    stop_df['trip_id'] = df['trip_id'] #TODO has to be filtered with only trains? could be reduce to exa num only
+    stop_df['station_id'] = df['stop_id']
+    stop_df['stop_sequence'] = df['stop_sequence']
+    stop_df.insert(0, 'stop_id', range(1, len(stop_df) + 1))
 
     with open(os.getenv('PATH_TO_STOP_INPUT'), 'w') as file:
-        for row in new_df.itertuples(index=False):
+        file.write(f'%% stop(Stop_id, Trip_id, Station_id, Stop_sequence).\n\n')
+        for row in stop_df.itertuples(index=False):
             encoded_row = f'stop({row.stop_id}, {row.trip_id}, {row.station_id}, {row.stop_sequence}).\n'
             file.write(encoded_row)
 
