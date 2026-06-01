df0d549afd84521dd2ab7add16471b67f773011c
MarianoVIsabella <marianovisabella@libero.it>
2026-06-01 11:16:51 +0200

    Added All/No Trip buttons

--- src/scripts/map.py
+++ src/scripts/map.py
@@ -338,7 +338,39 @@ def main():
     </div>
     """
     mappa.get_root().html.add_child(folium.Element(stats_html))
-    
+    #buttons to show every trip or to don't show any trip at all
+    buttons_html = """
+    <div style="position: fixed;
+                top: 10px; left: 50px;
+                z-index: 9999;
+                display: flex; gap: 8px;
+                font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">
+        <button onclick="toggleAllLayers(true)"
+                style="padding: 8px 14px; border-radius: 8px; border: none; cursor: pointer;
+                       background-color: silver; color: white; font-size: 13px; font-weight: 600;
+                       box-shadow: 0 2px 8px rgba(0,0,0,0.3);">
+            Tutte le corse
+        </button>
+        <button onclick="toggleAllLayers(false)"
+                style="padding: 8px 14px; border-radius: 8px; border: none; cursor: pointer;
+                       background-color: silver; color: white; font-size: 13px; font-weight: 600;
+                       box-shadow: 0 2px 8px rgba(0,0,0,0.3);">
+            Nessuna corsa
+        </button>
+    </div>
+    <script>
+        function toggleAllLayers(show) {
+            document.querySelectorAll('.leaflet-control-layers-overlays input[type=checkbox]').forEach(function(cb) {
+                var label = cb.closest('label');
+                if (cb.checked !== show && !label.innerText.trim().startsWith('Stazioni')){
+                    cb.click();
+                    } 
+            });
+        }
+    </script>
+    """
+    mappa.get_root().html.add_child(folium.Element(buttons_html))
+ 
     mappa.save(map_path)
     print(f"Mappa generata e salvata in: {map_path}")
 
